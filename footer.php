732f72445087701fbb78ac8df0c56d663b22f885
baotroy <baotroy@gmail.com>
2015-06-17 01:26:11 +0700

    event pg

---
+++ footer.php
@@ -0,0 +1,55 @@
+            <footer class="motopress-wrapper footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="span12" data-motopress-wrapper-file="wrapper/wrapper-footer.php" data-motopress-wrapper-type="footer" data-motopress-id="557f8b9b26f78">
+                            <div class="row footer-widgets">
+                                <div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-1">
+                                </div>
+                                <div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-2">
+                                </div>
+                                <div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-3">
+                                </div>
+                                <div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-4">
+                                </div>
+                            </div>
+                            <div class="row copyright">
+                                <div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-footer-text.php">
+                                    <div id="footer-text" class="footer-text">
+                                        <span class="footer_logo">Zoom</span> © 2015 | <a href="http://livedemo00.template-help.com/wordpress_46026/privacy-policy/" title="Privacy Policy">Privacy Policy</a>
+
+                                    </div> </div>
+                                <div class="span6 social-nets-wrapper" data-motopress-type="static" data-motopress-static-file="static/static-social-networks.php">
+                                    <div class="social_holder">
+                                        <h3>
+                                            Follow us </h3>
+                                        <ul class="social">
+                                            <li><a href="#" title="twitter"><img src="http://livedemo00.template-help.com/wordpress_46026/wp-content/themes/theme46026/images/social/twitter.png" alt="twitter"></a></li><li><a href="#" title="instagram"><img src="http://livedemo00.template-help.com/wordpress_46026/wp-content/themes/theme46026/images/social/instagram.png" alt="instagram"></a></li> </ul>
+                                    </div> </div>
+                                <div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-footer-nav.php">
+                                </div>
+                            </div> </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <div id="back-top-wrapper" class="visible-desktop">
+            <p id="back-top" style="display: none;">
+                <a href="#top"><span></span></a>
+            </p>
+        </div>
+        <script type="text/javascript" src="js/jquery.form.min.js?ver=3.32.0-2013.04.03"></script>
+        <script type="text/javascript">
+            /* <![CDATA[ */
+            var _wpcf7 = {"loaderUrl": "http:\/\/livedemo00.template-help.com\/wordpress_46026\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif", "sending": "Sending ..."};
+            /* ]]> */
+        </script>
+        <script type="text/javascript" src="js/scripts.js?ver=3.4"></script>
+        <script type="text/javascript" src="CherryFramework/js/superfish.js?ver=1.5.3"></script>
+        <script type="text/javascript" src="CherryFramework/js/jquery.mobilemenu.js?ver=1.0"></script>
+        <script type="text/javascript" src="CherryFramework/js/jquery.easing.1.3.js?ver=1.3"></script>
+        <script type="text/javascript" src="CherryFramework/js/jquery.magnific-popup.min.js?ver=0.9.3"></script>
+        <script type="text/javascript" src="CherryFramework/js/jquery.flexslider.js?ver=2.1"></script>
+        <script type="text/javascript" src="CherryFramework/js/camera.min.js?ver=1.3.4"></script>
+</body>
+</html>
